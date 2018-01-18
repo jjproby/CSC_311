fa210daf4c0d0564383e99a182a316534f68439d
Jordan Proby <jproby19@csl12-l.cornellcollege.edu>
2018-01-18 10:50:46 -0600

    Finally added documentation to the random number generator. All is good.

---
+++ RandNumGen/html/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['randnumgen_2ec',['randNumGen.c',['../rand_num_gen_8c.html',1,'']]]
+];
---
+++ RandNumGen/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['lcg',['lcg',['../rand_num_gen_8c.html#a961314425d451afd2d03cb36b0b74937',1,'randNumGen.c']]]
+];
