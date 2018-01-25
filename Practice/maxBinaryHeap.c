ee96ea3ecb07aff695823943ef0e024e3037c883
Jordan Proby <jproby19@csl19-l.cornellcollege.edu>
2018-01-24 21:31:40 -0600

    Somewhat getting through the project. Got it sorting with priority! Yay!

--- Practice/maxBinaryHeap.c
+++ Practice/maxBinaryHeap.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include  <limits.h>
 #define MAX 20
 void maxheapify(int *, int, int);
 int* buildmaxheap(int *, int);
@@ -40,4 +41,34 @@ void maxheapify(int a[], int i, int heapsize){
             maxheapify(a, largest, heapsize);
         }
     }
+
 }
+
+int main(){
+  int i, t, n;
+  int *a = calloc(MAX, sizeof(int));
+  int *m = calloc(MAX, sizeof(int));
+  printf("Enter no of elements in the array\n");
+  scanf("%d", &n);
+  printf("Enter the array\n");
+  for (i = 0; i < n; i++) {
+      a[i] = 5 + i;
+      //scanf("%d", &a[i]);
+    }
+  m = buildmaxheap(a, n);
+
+/*int balls[n +1];
+for (int fuck = 0; fuck < n+1;fuck++){
+  printf("val of fuck%d\n", m[n-fuck]);
+  balls[fuck] = m[n-fuck];
+}
+m = balls;*/
+
+  printf("The heap is\n");
+  /*for (t = n; t >= 0; t--) {
+      printf("%d\n", m[t]);
+    }*/
+  for (t = 0; t < n; t++) {
+        printf("%d\n", m[t]);
+    }
+  }
--- Practice/programSim.c
+++ Practice/programSim.c
@@ -1,57 +1,98 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include "maxBinaryHeap.c"
+#include <time.h>
 
-struct Program {
-  int arrivalTime;
-  int burstTime;
-  int waitTime;
-};
 
-void initProg(struct Program *prog) {
-  prog->arrivalTime = 20;
-  return;
+typedef struct {
+    double priority;
+    char *data;
+    double arrivalTime;
+    double waitTime;
+} node_t;
+
+typedef struct {
+    node_t *nodes;
+    int len;
+    int size;
+} heap_t;
+
+void push (heap_t *h, double priority, char *data) {
+    if (h->len + 1 >= h->size) {
+        h->size = h->size ? h->size * 2 : 4;
+        h->nodes = (node_t *)realloc(h->nodes, h->size * sizeof (node_t));
+    }
+    int i = h->len + 1;
+    int j = i / 2;
+    while (i > 1 && h->nodes[j].priority > priority) {
+        h->nodes[i] = h->nodes[j];
+        i = j;
+        j = j / 2;
+    }
+    h->nodes[i].priority = priority;
+    h->nodes[i].data = data;
+    h->len++;
 }
 
+char *pop (heap_t *h) {
+    int i, j, k;
+    if (!h->len) {
+        return NULL;
+    }
+    char *data = h->nodes[1].data;
+
+    h->nodes[1] = h->nodes[h->len];
+    int priority = h->nodes[1].priority;
+    h->len--;
+    i = 1;
+    while (1) {
+        k = i;
+        j = 2 * i;
+        if (j <= h->len && h->nodes[j].priority < priority) {
+            k = j;
+        }
+        if (j + 1 <= h->len && h->nodes[j + 1].priority < h->nodes[k].priority) {
+            k = j + 1;
+        }
+        if (k == i) {
+            break;
+        }
+        h->nodes[i] = h->nodes[k];
+        i = k;
+    }
+    h->nodes[i] = h->nodes[h->len + 1];
+    double *priority2 = &h->nodes[1].priority;
+    printf("Priority: %lf\n", *priority2);
+    return data;
+}
+
+
 int main(){
-  /*int i, t, n;
-  int *a = calloc(MAX, sizeof(int));
-  int *m = calloc(MAX, sizeof(int));
-  printf("Enter no of elements in the array\n");
-  scanf("%d", &n);
-  printf("Enter the array\n");
-  for (i = 0; i < n; i++) {
-      scanf("%d", &a[i]);
-    }
-  m = buildmaxheap(a, n);
-  printf("The heap is\n");
-  for (t = 0; t < n; t++) {
-      printf("%d\n", m[t]);
-    }*/
+    srand(time(NULL));
+    int j;
+    heap_t *h = (heap_t *)calloc(2, sizeof (heap_t));
+    for (j = 0; j < 5; j++) {
+      node_t *t = (node_t *)calloc(2, sizeof (node_t));
+      char * d = calloc(2, sizeof (char *));
+      t->arrivalTime = 0;
+      t->priority = rand();
+      t->waitTime = 0;
+      *d = j+65;
 
-    struct Program program1;
-    struct Program program2;
-    struct Program program3;
+      push(h, t->priority, d);
 
-    program1.burstTime = 25;
-    program2.burstTime = 35;
-
-    program1.arrivalTime = 0;
-    program2.arrivalTime = 1;
-    program3.arrivalTime =2;
-
-    program1.waitTime=0;
-    program1.waitTime=0;
-    //printf("hello");
-
-    if(program1.burstTime < program2.burstTime) {
-      program2.arrivalTime = program2.arrivalTime + program1.burstTime;
-      program2.waitTime = program2.waitTime + program1.burstTime;
-      printf("Program 1 Arrival Time: %d, Burst Time: %d, Waiting Time: %d\n", program1.arrivalTime, program1.burstTime, program1.waitTime);
-      printf("Program 2 Arrival Time: %d, Burst Time: %d, Waiting Time: %d\n", program2.arrivalTime, program2.burstTime, program2.waitTime);
     }
 
-    initProg(&program3);
-    printf(" Arrival Time : %d", program3.arrivalTime);
+    /*push(h, 3, "Clear drains");
+    push(h, 4, "Feed cat");
+    push(h, 5, "Make tea");
+    push(h, 1, "Solve RC tasks");
+    push(h, 2, "Tax return");*/
+    int i;
+    for (i = 0; i < 5; i++) {
+        printf("%s\n", pop(h));
+    }
+    return 0;
+
 }
