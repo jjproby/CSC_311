98aac7989a02ea448981f959f51c646886fe5dd9
Jordan Proby <jproby19@csl12-l.cornellcollege.edu>
2018-02-05 11:25:58 -0600

    IT FINALLY WORKSgit add .! LET'S GO!

--- WordReader/wordRead.c
+++ WordReader/wordRead.c
@@ -6,40 +6,29 @@
 #include <time.h>
 
 int main( int argc, char** argv ) {
-  srand(time(NULL));
   char *line = NULL;
-  size_t lengthOfBuffer = 0;
-  ssize_t numberOfCharactersRead;
-  int letters[26] = {0};
-  int newLetters[26] = {0};
-  int chars = 0;
-  double percent[26] = {0};
-  double randPercent[26] = {0};
-  int *p;
   char x[1024];//string array
   int num = 0;
 
-
   FILE * inputStream = fopen( "frankenstein.txt", "r" );
-
-  //numberOfCharactersRead = getline(&line, &lengthOfBuffer, inputStream);
 
   while (fscanf(inputStream, " %s", x) == 1) {
     //puts(x);
     num++;
   }
 
+  //printf("%d\n", num);
+
   fclose( inputStream );
 
   FILE * inputStream2 = fopen( "frankenstein.txt", "r" );
 
   char *y[num];//array of words
-  int wordNumber[num];
   char *singleWords[num];
+  int uniqueWords = 0;
 
   for (int i = 0; i < num; i++) {
-    char *z[num];
-    wordNumber[i] = 0;
+    //wordNumber[i] = 0;
     if (fscanf(inputStream2, " %s", x) != 1) {
       break;
     }
@@ -47,62 +36,39 @@ int main( int argc, char** argv ) {
     for (int j=0; x[j]; j++){
       x[j] = tolower(x[j]);
     }
-    z[i] = strdup(x);
-    /*for (int k = 0; k < strlen(y[i]); k++) {
-      if (strcmp(y[k], z[i]) == 0) {
-        wordNumber[k]++;
-        continue;
-      }
-    }*/
     y[i] = strdup(x);
-    wordNumber[i]++;
-    printf("%s\n", y[i]);
+    //wordNumber[i]++;
   }
 
   fclose( inputStream2 );
 
-  for (int i = 0; i < num; i++) {
+  for (int i = 1; i < num; i++) {
     for (int j = 0; j < i; j++) {
       if (strcmp(y[j], y[i]) == 0) {
-        wordNumber[j]++;
+        break;
       }
-      else {
-        singleWords[i] = strdup(y[i]);
+      if (j == i - 1) {
+        singleWords[uniqueWords] = strdup(y[i]);
+        uniqueWords++;
       }
     }
   }
 
-  /*for (int i = 0; i < num; i++) {
-    printf("%s\n", y[i]);
-  }*/
+  int wordNumber[uniqueWords];
 
-  /*for (int i = 0; i < num; i++) {
-    /*for (int j = 0; j < i; j++) {
-      if (strcmp(y[j], y[i]) == 0) {
-        continue;
-      }//if
-    }//for
-    FILE * inputStream3 = fopen( "frankenstein.txt", "r" );
+  for (int i = 0; i < uniqueWords; i++){
+    wordNumber[i] = 0;
     for (int j = 0; j < num; j++) {
-      if (fscanf(inputStream, " %s", x) != 1) {
-        break;
-      }//if
-      //puts(x);
-      for (int k=0; x[k]; k++){
-        x[k] = tolower(x[k]);
-      }//for
-      z[i] = strdup(x);
-      if (strcmp(y[i], z[i]) == 0) {
+      if (strcmp(y[j], singleWords[i]) == 0) {
         wordNumber[i]++;
-      }//if
-    }//for
-    fclose( inputStream3 );
-    read++;
-  }//for*/
-
-  for (int i = 0; i < num; i++) {
-    printf("%s : %d \n", y[i], wordNumber[i]);
+      }
+    }
   }
+
+  for (int i = 0; i < uniqueWords; i++) {
+    printf("%s : %d \n", singleWords[i], wordNumber[i]);
+  }
+  printf("Unique words: %d\n", uniqueWords);
 
 
   printf("Numebr of words: %d\n", num);
